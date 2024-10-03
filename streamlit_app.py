468b20f21c6cd38149a7c35082058669812186c8
adashino23sky <137061803+adashino23sky@users.noreply.github.com>
2024-10-03 21:12:12 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,31 @@
 import streamlit as st
 from openai import OpenAI
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+
+from langchain_core.messages import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
+
+
+messages = [
+    SystemMessage("you're a good assistant, you always respond with a joke."),
+    HumanMessage(content = human_input),
+    AIMessage(
+        content = ai_output
+    ),
+    HumanMessage("and who is harrison chasing anyways"),
+    AIMessage(
+        "Hmmm let me think.\n\nWhy, he's probably chasing after the last cup of coffee in the office!"
+    ),
+    HumanMessage("what do you call a speechless parrot"),
+]
+
+
+import firebase_admin
+from firebase_admin import credentials
 
 # Show title and description.
 st.title("")
@@ -8,8 +34,21 @@ openai_api_key = st.secret("openAIAPI.key")
 if not openai_api_key:
     st.info("Please add your OpenAI API key to secret to continue.", icon="🗝️")
 else:
-    # client作成
+    # APIクライアント作成
     client = OpenAI(api_key=openai_api_key)
+    
+    llm = ChatOpenAI(model="gpt-4o-mini")
+
+    # システムプロンプト作成
+    with open('prompt.txt', encoding = 'UTF-8', mode = 'r') as f:
+        system_template = f.read()
+
+    # # 会話のテンプレートを作成
+    prompt = ChatPromptTemplate.from_messages([
+        SystemMessagePromptTemplate.from_template(system_template),
+        MessagesPlaceholder(variable_name="history"),
+        HumanMessagePromptTemplate.from_template("{input}"),
+])
     # 会話格納コンテナ初期化
     if "messages" not in st.session_state:
         st.session_state.messages = []
@@ -20,12 +59,12 @@ else:
             st.markdown(message["content"])
 
     # 会話入力
-    if prompt := st.chat_input("あなたの意見を入力してください"):
+    if input_message := st.chat_input("あなたの意見を入力してください"):
 
         # Store and display the current prompt.
         st.session_state.messages.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
-            st.markdown(prompt)
+            st.markdown(input_message)
 
         # Generate a response using the OpenAI API.
         stream = client.chat.completions.create(
@@ -42,3 +81,7 @@ else:
         with st.chat_message("assistant"):
             response = st.write_stream(stream)
         st.session_state.messages.append({"role": "assistant", "content": response})
+
+    # firebase認証
+    cred = credentials.Certificate("path/to/serviceAccountKey.json")
+    firebase_admin.initialize_app(cred)
